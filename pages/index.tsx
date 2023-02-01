98ddb1f2090b37d9a3b6bf68ad38086a908891c4
Koji Kishimoto <master1223meister@gmail.com>
2023-02-01 14:24:52 +0900

    feat: !の追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
+            !!!!Get started by editing!!!&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
           </p>
           <div>
